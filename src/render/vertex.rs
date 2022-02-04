59fac0d9f4040bbf9d44b989ce99d9f4377df6d3
PersonBelowRocks <ulrikwithc@gmail.com>
2022-02-04 12:37:13 +0100

    whoops forgot this

--- src/render/vertex.rs
+++ src/render/vertex.rs
@@ -59,3 +59,67 @@ const NORTH: FaceVectors = [
     Vector3::new(-0.5, 0.5, -0.5),
     Vector3::new(0.5, -0.5, -0.5)
 ];
+
+/*
+pub const _VERTICES: [Vertex; 36] = [
+    // Face 1
+    // 0, 7, 5
+    Vertex { position: (0.5, 0.5, 0.5) },
+    Vertex { position: (0.5, -0.5, 0.5) },
+    Vertex { position: (0.5, 0.5, -0.5) },
+    // 4, 7, 5
+    Vertex { position: (0.5, -0.5, -0.5) },
+    Vertex { position: (0.5, -0.5, 0.5) },
+    Vertex { position: (0.5, 0.5, -0.5) },
+
+    // Face 2
+    // 5, 6, 4
+    Vertex { position: (0.5, 0.5, -0.5) },
+    Vertex { position: (-0.5, 0.5, -0.5) },
+    Vertex { position: (0.5, -0.5, -0.5) },
+    // 3, 6, 4
+    Vertex { position: (-0.5, -0.5, -0.5) },
+    Vertex { position: (-0.5, 0.5, -0.5) },
+    Vertex { position: (0.5, -0.5, -0.5) },
+
+    // Face 3
+    // 6, 1, 3
+    Vertex { position: (-0.5, 0.5, -0.5) },
+    Vertex { position: (-0.5, 0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, -0.5) },
+    // 2, 3, 1
+    Vertex { position: (-0.5, -0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, -0.5) },
+    Vertex { position: (-0.5, 0.5, 0.5) },
+
+    // Face 4
+    // 1, 0, 2
+    Vertex { position: (-0.5, 0.5, 0.5) },
+    Vertex { position: (0.5, 0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, 0.5) },
+    // 7, 0, 2
+    Vertex { position: (0.5, -0.5, 0.5) },
+    Vertex { position: (0.5, 0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, 0.5) },
+
+    // Face 5
+    // 1, 0, 6
+    Vertex { position: (-0.5, 0.5, 0.5) },
+    Vertex { position: (0.5, 0.5, 0.5) },
+    Vertex { position: (-0.5, 0.5, -0.5) },
+    // 5, 0, 6
+    Vertex { position: (0.5, 0.5, -0.5) },
+    Vertex { position: (0.5, 0.5, 0.5) },
+    Vertex { position: (-0.5, 0.5, -0.5) },
+
+    // Face 6
+    // 2, 3, 7
+    Vertex { position: (-0.5, -0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, -0.5) },
+    Vertex { position: (0.5, -0.5, 0.5) },
+    // 4, 7, 3
+    Vertex { position: (0.5, -0.5, -0.5) },
+    Vertex { position: (0.5, -0.5, 0.5) },
+    Vertex { position: (-0.5, -0.5, -0.5) },
+];
+ */
