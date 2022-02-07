4944e7f28fd848cd50d5f399a2096ae04fa3d854
PersonBelowRocks <ulrikwithc@gmail.com>
2022-02-07 12:06:08 +0100

    Camera move system

--- src/main.rs
+++ src/main.rs
@@ -1,270 +1,103 @@
-mod world;
-mod render;
-mod util;
+use std::f32::consts::PI;
 
-use std::time::Duration;
-use wgpu::{PolygonMode, PrimitiveTopology};
-use wgpu::util::DeviceExt;
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder, Window},
-};
+use bevy::prelude::*;
+use bevy::input::mouse::MouseMotion;
 
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-    color: [f32; 3]
-}
+const MOUSE_SENSITIVITY: f32 = 0.05;
 
-impl Vertex {
-    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                }
-            ]
-        }
-    }
-}
+#[derive(Component)]
+struct Player;
 
-#[allow(unused)]
-const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.25, 1.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] }
-];
-
-struct State {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer: wgpu::Buffer,
-    num_vertices: u32
-}
-
-impl State {
-    async fn new(window: &Window) -> Self {
-        let size = window.inner_size();
-
-        let instance = wgpu::Instance::new(wgpu::Backends::all());
-        let surface = unsafe { instance.create_surface(window) };
-        let adapter = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false
-            }
-        ).await.unwrap();
-
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
-                label: None
-            },
-            None
-        ).await.unwrap();
-
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface.get_preferred_format(&adapter).unwrap(),
-            width: size.width,
-            height: size.height,
-            present_mode: wgpu::PresentMode::Fifo
-        };
-
-        surface.configure(&device, &config);
-
-        let shader =
-            device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-                label: Some("Shader"),
-                source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-            });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline =
-            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                label: Some("Render Pipeline"),
-                layout: Some(&render_pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: "vs_main",
-                    buffers: &[Vertex::desc()],
-                },
-                fragment: Some(wgpu::FragmentState {
-                    module: &shader,
-                    entry_point: "fs_main",
-                    targets: &[wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    }],
-                }),
-                primitive: wgpu::PrimitiveState {
-                    cull_mode: None,
-                    front_face: wgpu::FrontFace::Cw,
-                    topology: PrimitiveTopology::TriangleList,
-                    polygon_mode: PolygonMode::Fill,
-                    unclipped_depth: false,
-                    conservative: false,
-                    strip_index_format: None,
-                },
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false,
-                },
-                multiview: None
-            });
-
-        let vertex_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(VERTICES),
-                usage: wgpu::BufferUsages::VERTEX,
-            }
-        );
-
-        Self {
-            surface,
-            device,
-            queue,
-            config,
-            size,
-            render_pipeline,
-            vertex_buffer,
-            num_vertices: VERTICES.len() as u32,
-        }
-    }
-
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if new_size.height > 0 && new_size.width > 0 {
-            self.size = new_size;
-            self.config.height = self.size.height;
-            self.config.width = self.size.width;
-
-            self.surface.configure(&self.device, &self.config);
-        }
-    }
-
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::KeyboardInput {
-                input: KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Space), ..
-                }, ..
-            } => {
-                false
-            },
-            _ => false
-        }
-    }
-
-    fn update(&mut self) {
-        // todo!()
-    }
-
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view =
-            output.texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        let mut encoder = self.device.create_command_encoder(
-            &wgpu::CommandEncoderDescriptor { label: Some("Render Encoder") });
-
-
-        let mut render_pass = encoder.begin_render_pass(
-            &wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                depth_stencil_attachment: None,
-                color_attachments: &[wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        store: true,
-                        load: wgpu::LoadOp::Clear(wgpu::Color {r: 0.3, g: 0.7, b: 0.9, a: 1.0})
-                    }
-                }]
-            }
-        );
-
-        render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-        render_pass.draw(0..self.num_vertices, 0..1);
-
-        drop(render_pass);
-
-        self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
-
-        Ok(())
-    }
-}
+type Rotation = (f32, f32);
 
 fn main() {
-    env_logger::init();
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(keyboard_controls)
+        .add_system(mouse_controls)
+        .run();
+}
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().with_title("Hello WGPU").build(&event_loop).unwrap();
-    let mut state = pollster::block_on(State::new(&window));
+fn mouse_controls(mut rot: Local<Rotation>, mut events: EventReader<MouseMotion>, mut player: Query<&mut Transform, With<Player>>) {
+    let mut pitch: f32 = 0.0;
+    let mut yaw: f32 = 0.0;
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => if !state.input(event) {
-            match event {
-                WindowEvent::CloseRequested
-                | WindowEvent::KeyboardInput {
-                    input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                    ..
-                } => *control_flow = ControlFlow::Exit,
+    for mouse in events.iter() {
+        pitch += mouse.delta.y * MOUSE_SENSITIVITY;
+        yaw += -mouse.delta.x * MOUSE_SENSITIVITY;
+    }
 
-                WindowEvent::Resized(new_size) =>
-                    state.resize(*new_size),
-                WindowEvent::ScaleFactorChanged { new_inner_size, .. } =>
-                    state.resize(**new_inner_size),
-                _ => {}
-            }
-        },
-        Event::RedrawRequested(window_id) if window_id == window.id() => {
-            state.update();
-            match state.render() {
-                Ok(_) => {},
-                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                Err(e) => eprintln!("{:?}", e),
-            }
-        },
-        Event::MainEventsCleared => window.request_redraw(),
-        _ => {}
+    let new_pitch = rot.0 + (pitch * PI/180.0);
+    if !(new_pitch > PI/2.0 || new_pitch < -PI/2.0) {
+        rot.0 = new_pitch;
+    }
+    rot.1 += yaw * PI/180.0;
+
+
+    let mut trans = player.single_mut();
+    trans.rotation = Quat::from_axis_angle(Vec3::Y, rot.1) * Quat::from_axis_angle(-Vec3::X, rot.0);
+}
+
+fn keyboard_controls(kb: Res<Input<KeyCode>>, mut player: Query<&mut Transform, With<Player>>) {
+    let mut trans = player.single_mut();
+    let local_fwd = trans.forward();
+    let local_right = trans.right();
+
+    if kb.pressed(KeyCode::Space) {
+        trans.translation.y += 0.125;
+    }
+    if kb.pressed(KeyCode::LShift) {
+        trans.translation.y -= 0.125;
+    }
+    if kb.pressed(KeyCode::W) {
+        trans.translation += local_fwd * 0.125;
+    }
+    if kb.pressed(KeyCode::S) {
+        trans.translation -= local_fwd * 0.125;
+    }
+    if kb.pressed(KeyCode::D) {
+        trans.translation += local_right * 0.125;
+    }
+    if kb.pressed(KeyCode::A) {
+        trans.translation -= local_right * 0.125;
+    }
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
     });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+    // camera
+    
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    }).insert(Player);
 }
